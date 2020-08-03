7578802eaf33914da22b1ff06628d9f000c6c665
TsundereKermit <tonyjiang917@gmail.com>
2020-08-03 15:24:50 -0400

    monkaS

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -12,12 +13,21 @@ const db = require("./config/keys").mongoURI;
 
 //Connect to MongoDB
 mongoose
-    .connect(db, { useNewUrlParser:true, useUnifiedTopology:true })
-    .then(() => console.log("MongoDB Connected."))
-    .catch(err => console.error(err));
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected."))
+  .catch((err) => console.error(err));
 
 //Use routes
 app.use("/", require("./routes/api/sticky.js"));
+
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
