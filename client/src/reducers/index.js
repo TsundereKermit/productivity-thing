8334aecdc4558d3e595bf434b5cec6b32757ce5f
TsundereKermit <tonyjiang917@gmail.com>
2020-08-10 15:23:12 -0400

    implemented lesson 10

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import stickyReducer from "./stickyReducer";
+import errorReducer from "./errorReducer";
+import authReducer from "./authReducer";
 
 export default combineReducers({
   sticky: stickyReducer,
+  error: errorReducer,
+  auth: authReducer,
 });
--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ function auth(req, res, next) {
   const token = req.header("x-auth-token");
 
   if (!token) {
-    res.status(401).json({ msg: "No token, auth denied" });
+    return res.status(401).json({ msg: "No token, auth denied" });
   }
 
   try {
